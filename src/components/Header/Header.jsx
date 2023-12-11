2cfd7f13a07bbc170461478251fbb31011b52b05
KyawZinLin3 <kyawzinlin14future@gmail.com>
2023-12-11 23:32:35 +0630

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Typography, Toolbar, InputBase, Box } from "@material-ui/core";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <AppBar position="static">
+      <Toolbar className="">
+        <Typography variant="h5"></Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
